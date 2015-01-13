8fb52bce036c16a21d5c7a5ef7da3524f641c5d5
Brandon Morrison <brandon@brandonmorrison.com>
2015-01-13 18:57:37 -0500

    UNRW-477: Buildout of the crisis overview widget. Added styling, markup from patternlab

--- src/components/crisis-overview/crisis-overview.js
+++ src/components/crisis-overview/crisis-overview.js
@@ -21,4 +21,26 @@ var CrisisOverviewWidget = function(opts) {
 
 CrisisOverviewWidget.prototype = new WidgetBase();
 
+CrisisOverviewWidget.prototype.render = function(element) {
+  var config = this.config();
+  this.config('adjustedTitle', titleAdjust(config.title));
+
+  this.template(function(content) {
+    d3.select(element)
+      .classed('rw-widget', true)
+      .classed('rw-widget-image', true)
+      .html(content);
+  });
+};
+
+function titleAdjust(title) {
+  var snippet = '<span class="word[[counter]]">[[word]]</span>';
+  var words = title.split(' ');
+  var adjustedTitle = '';
+  for (var i = 0; i < words.length; i++) {
+    adjustedTitle += snippet.replace('[[counter]]', i + 1).replace('[[word]]', words[i]);
+  }
+  return adjustedTitle;
+}
+
 module.exports = CrisisOverviewWidget;
