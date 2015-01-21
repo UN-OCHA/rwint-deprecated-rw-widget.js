c1f3abbce776bd3ca95a034bfb08e92bfbd5f2f4
Brandon Morrison <brandon@brandonmorrison.com>
2015-01-21 11:34:48 -0500

    Refactoring timeline js to be more modular. Adjust default grunt task to copy dist files before watching

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -22,11 +22,17 @@ TimelineWidget.prototype = new WidgetBase();
 
 TimelineWidget.prototype.link = function(elements) {
   var timelineState = {};
+  var timelineContent = this.config('timeline-items');
+
   var $element = $(elements[0][0]); // @TODO, grab any potential element selected.
   var $frame,
       $item,
       $margin,
       $index;
+
+  var $sly,
+      $slyPager,
+      $slyDropdown;
 
   function init() {
     var now = moment();
@@ -35,6 +41,85 @@ TimelineWidget.prototype.link = function(elements) {
     timelineState.currentMonth = now.format('M');
     timelineState.currentFormatted = now.format('YYYY MMMM');
 
+    populateOverlaySelectors();
+
+    $('select', $element).selectric();
+    // Initialize Sly Sliders.
+    $frame = $('.timeline-widget-frames', $element);
+    $item = $('.timeline-widget-item', $element);
+    $margin = '40px';
+
+    // Set initial widths.
+    adjustTimelineWidth($frame.width());
+    $item.css({ marginRight : $margin});
+
+    // Control resizing.
+    $(window).resize(function(e) {
+      adjustTimelineWidth($frame.width());
+    });
+
+    // Main slider.
+    $sly = new Sly($frame, {
+      horizontal: 1,
+      itemNav: 'forceCentered',
+      smart: 1,
+      activateMiddle: 1,
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: 0,
+      scrollBy: 1,
+      speed: 200,
+      elasticBounds: 1,
+      dragHandle: 1,
+      dynamicHandle: 1,
+      clickBar: 1,
+
+      // Buttons
+      prev: $('.prev'),
+      next: $('.next')
+    }).init();
+
+    // Pager.
+    $slyPager = new Sly($('.timeline-widget-pager', $element), {
+      horizontal: 1,
+      itemNav: 'forceCentered',
+      smart: 1,
+      activateMiddle: 1,
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: 0,
+      scrollBy: 1,
+      speed: 200,
+      elasticBounds: 1,
+      dragHandle: 1,
+      dynamicHandle: 1,
+      clickBar: 1
+    }).init();
+
+    // Dropdowns.
+    $slyDropdown = new Sly($('.timeline-widget--dropdown--container', $element), {
+      itemNav: 'basic',
+      smart: 1,
+      activateOn: 'click',
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: 0,
+      scrollBy: 1,
+      activatePageOn: 'click',
+      speed: 300,
+      elasticBounds: 1,
+      dragHandle: 1,
+      dynamicHandle: 1,
+      clickBar: 1
+    }).init();
+
+    paint();
+  }
+
+  function populateOverlaySelectors() {
     var months = moment.months();
     var monthsShort = moment.monthsShort('-MMM-');
     var $monthSelector = $('select[name="month"]', $element);
@@ -49,97 +134,34 @@ TimelineWidget.prototype.link = function(elements) {
       $yearSelector.append('<option value="' + year + '">' + year + '</option>');
       year--;
     }
-
-    $('select', $element).selectric();
-    // Initialize Sly Sliders.
-    $frame = $('.timeline-widget-frames', $element);
-    $item = $('.timeline-widget-item', $element);
-    $margin = '40px';
-
-
-
-    paint();
   }
 
   function paint() {
     $element.find('.timeline-widget-pager--current').text(timelineState.currentFormatted);
   }
 
+  function slideTo(index) {
+    var $sliderPos = $sly.getPos(index);
+    $sly.slideTo($sliderPos.center);
+
+    var $pagerPos = $slyPager.getPos(index);
+    $slyPager.slideTo($pagerPos.center);
+
+    var $dropDownPos = $slyDropdown.getPos(index);
+    $slyDropdown.slideTo($dropDownPos.start);
+  }
+
+  function adjustTimelineWidth(width) {
+    $item.width(width);
+    $('.timeline-widget-pager li', $element).width(Math.floor(width/3));
+
+    if ($sly && $slyPager) {
+      $sly.reload();
+      $slyPager.reload();
+    }
+  }
+
   init();
-
-  // Set initial widths.
-  var $initialWidth = $frame.width();
-  var $width = $initialWidth;
-  $item.width($initialWidth);
-  $item.css({ marginRight : $margin});
-  $('.timeline-widget-pager li').width(Math.floor($initialWidth/3));
-
-  // Control resizing.
-  $(window).resize(function(e) {
-    $width = $frame.width();
-    $item.width($width);
-    $sly.reload();
-    $('.timeline-widget-pager li').width(Math.floor($width/3));
-    $slyPager.reload();
-  });
-
-  // Main slider.
-  var $sly = new Sly($frame, {
-    horizontal: 1,
-    itemNav: 'forceCentered',
-    smart: 1,
-    activateMiddle: 1,
-    mouseDragging: 1,
-    touchDragging: 1,
-    releaseSwing: 1,
-    startAt: 0,
-    scrollBy: 1,
-    speed: 200,
-    elasticBounds: 1,
-    dragHandle: 1,
-    dynamicHandle: 1,
-    clickBar: 1,
-
-    // Buttons
-    prev: $('.prev'),
-    next: $('.next')
-  }).init();
-
-  // Pager.
-  var $slyPager = new Sly('.timeline-widget-pager', {
-    horizontal: 1,
-    itemNav: 'forceCentered',
-    smart: 1,
-    activateMiddle: 1,
-    mouseDragging: 1,
-    touchDragging: 1,
-    releaseSwing: 1,
-    startAt: 0,
-    scrollBy: 1,
-    speed: 200,
-    elasticBounds: 1,
-    dragHandle: 1,
-    dynamicHandle: 1,
-    clickBar: 1
-  }).init();
-
-  // Dropdowns.
-  var $slyDropdown = new Sly('.timeline-widget--dropdown--container', {
-    itemNav: 'basic',
-    smart: 1,
-    activateOn: 'click',
-    mouseDragging: 1,
-    touchDragging: 1,
-    releaseSwing: 1,
-    startAt: 0,
-    scrollBy: 1,
-    activatePageOn: 'click',
-    speed: 300,
-    elasticBounds: 1,
-    dragHandle: 1,
-    dynamicHandle: 1,
-    clickBar: 1
-  }).init();
 
   $('.timeline-widget-pager--item, .timeline-widget-dropdown--item').click(function(){
     $index = $(this).attr('data-slide');
@@ -159,16 +181,9 @@ TimelineWidget.prototype.link = function(elements) {
   });
 
   // Update other sliders based on main.
-  $sly.on('moveEnd', function(){
-    $index = $sly.rel.activeItem;
-    timelineState.currentIndex = $index;
-    console.log(timelineState, moment.months());
-
-    var $pagerPos = $slyPager.getPos($index);
-    $slyPager.slideTo($pagerPos.center);
-
-    var $dropDownPos = $slyDropdown.getPos($index);
-    $slyDropdown.slideTo($dropDownPos.start);
+  $sly.on('moveStart', function(){
+    timelineState.currentIndex = $sly.rel.activeItem;
+    slideTo(timelineState.currentIndex);
   });
 };
 
